e8bf080031955daa3b68c3b29729704e24f8fd66
Streidis <arispap50@gmail.com>
2025-05-11 19:22:36 +0300

    FIle creation

---
+++ MainServer/src/MainServer.java
@@ -0,0 +1,11 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public class MainServer {
+    public static void main(String[] args) throws Exception {
+        
+
+
+        
+    }
+}
