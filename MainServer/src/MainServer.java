ad15cff03a94f6944ec41a03b98eeba1c9d90524
Streidis <arispap50@gmail.com>
2025-05-11 19:35:56 +0300

    import delete

--- MainServer/src/MainServer.java
+++ MainServer/src/MainServer.java
@@ -1,11 +1,8 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
 public class MainServer {
     public static void main(String[] args) throws Exception {
         
 
 
-        
+
     }
 }
