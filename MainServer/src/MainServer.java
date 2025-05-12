109bc7d53b49c59fded09e079d474b456747121b
Streidis <arispap50@gmail.com>
2025-05-12 22:08:45 +0300

    MainServer 1st version

--- MainServer/src/MainServer.java
+++ MainServer/src/MainServer.java
@@ -1,8 +1,28 @@
+import java.net.*;
+import java.io.*;
+
 public class MainServer {
     public static void main(String[] args) throws Exception {
-        
+        try{
+            Socket sock = new Socket("localhost", 5550);
 
+            BufferedReader instream = new BufferedReader (new InputStreamReader(sock.getInputStream()));
+            BufferedWriter outstream = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
 
+            System.out.println("Connecting to "+ sock.getInetAddress()+ " and port "+sock.getPort());
+            System.out.println("Local Address :"+sock.getLocalAddress()+" Port:"+sock.getLocalPort());
+            String strin, strout;
+            strin = instream.readLine();
 
+            System.out.println("The time from server is: " + strin);
+
+            instream.close();
+            outstream.close();
+            sock.close();
+            System.out.println("Connection Closing...");
+        }
+        catch (IOException ex){
+            System.out.println("Connection Refused!!!");
+        }
     }
 }
