4c1698d8a83490a4f913b48e217c4596b0609066
kkkatsar <153008609+kkkatsar@users.noreply.github.com>
2025-05-12 18:56:58 +0000

    dbserver with sockets now

--- DBServer.java
+++ DBServer.java
@@ -1,20 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package dbserver;
+
+import java.io.*;
+import java.net.*;
 
 /**
  *
- * @author aisxi
+ * @author kkkatsar
  */
 public class DBServer {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            try (Socket sock = new Socket("localhost", 8888)) {
+                BufferedReader input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                BufferedWriter response = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+                System.out.println("Sending Messages to the Server...");
+                System.out.println("Connecting to " + sock.getInetAddress() + " and port " + sock.getPort());
+                System.out.println("Local Address :" + sock.getLocalAddress() + " Port:" + sock.getLocalPort());
+
+                /*
+                
+                code goes here at some point idk lel
+                
+                 */
+                input.close();
+                response.close();
+            }
+            System.out.println("Closing connection...");
+        } catch (IOException ex) {
+            System.err.println("Connection refused");
+        }
     }
-    
+
 }
